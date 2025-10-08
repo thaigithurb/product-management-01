22a6886eff8f7ca55b1ba4640db75bc30b5ae70a
thaigithurb <thaivietluong2005@gmail.com>
2025-10-08 21:49:49 +0700

    làm tính năng đăng xuất

--- controller/admin/auth.controller.js
+++ controller/admin/auth.controller.js
@@ -2,12 +2,14 @@ const Account = require("../../models/account.model");
 const systemConfig = require("../../config/system");
 const md5 = require('md5');
 
+//[GET] /admin/auth/login
 module.exports.login = (req, res) => {
     res.render("admin/pages/auth/login.pug", {
         pageTitle: "Đăng nhập"
     })
 }
 
+//[POST] /admin/auth/login
 module.exports.loginPost = async (req, res) => {
 
     const email = req.body.email;
@@ -38,4 +40,10 @@ module.exports.loginPost = async (req, res) => {
 
     res.cookie("token", user.token);
     res.redirect(`${systemConfig.prefixAdmin}/dashboard`);
+}
+
+//[GET] /admin/auth/logout
+module.exports.logout = (req, res) => {
+    res.clearCookie("token")
+    res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
 }
