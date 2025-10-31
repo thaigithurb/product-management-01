add80d5820b5159e5352cb66a9d067e79836e83c
thaigithurb <thaivietluong2005@gmail.com>
2025-10-31 11:09:21 +0700

    làm giao diện trang chat

---
+++ controller/client/chat.controller.js
@@ -0,0 +1,7 @@
+// [GET] / 
+module.exports.index = async (req, res) => {
+
+    res.render("client/pages/chat/index.pug", {
+        pageTitle: "Chat",
+    })
+}
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -4,6 +4,7 @@ const searchRoutes = require("./search.route");
 const cartRoutes = require("./cart.route");
 const userRoutes = require("./user.route");
 const checkoutRoutes = require("./checkout.route");
+const chatRoutes = require("./chat.route");
 const categoryMiddleware = require("../../middlewares/client/category.middleware");
 const cartMiddleware = require("../../middlewares/client/cart.middleware");
 const userMiddleware = require("../../middlewares/client/user.middleware");
@@ -30,5 +31,7 @@ module.exports = (app) => {
     app.use("/checkout", checkoutRoutes);
 
     app.use("/user", userRoutes);
+
+    app.use("/chat", chatRoutes);
 
 }
