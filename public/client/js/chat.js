5a1501250db6f1f20bf32dcfe62cc6ba36fe1b03
thaigithurb <thaivietluong2005@gmail.com>
2025-11-03 16:40:53 +0700

    chèn icon

--- public/client/js/chat.js
+++ public/client/js/chat.js
@@ -1,4 +1,5 @@
-
+import * as Popper from 'https://cdn.jsdelivr.net/npm/@popperjs/core@^2/dist/esm/index.js'
+import textFieldEdit from 'https://cdn.jsdelivr.net/npm/text-field-edit@^4/index.js'
 
 // CLIENT_SEND_MESSAGE 
 const formSendData = document.querySelector(".input-bar");
@@ -21,7 +22,6 @@ socket.on("SERVER_RETURN_MESSAGE", (data) => {
     const body = document.querySelector(".messages");
     const div = document.createElement("div");
     div.classList.add("message");
-    // Check if message is mine
     if (data.userId == myId) {
         div.classList.add("right");
         div.innerHTML = `
@@ -35,8 +35,27 @@ socket.on("SERVER_RETURN_MESSAGE", (data) => {
         `;
     }
     body.appendChild(div);
-
-    // Auto scroll to bottom
     body.scrollTop = body.scrollHeight;
 })
 // END SERVER_RETURN_MESSAGE 
+
+// show icon chat 
+document.querySelector('emoji-picker')
+    .addEventListener('emoji-click', event => console.log(event.detail));
+
+const buttonIcon = document.querySelector('.button-icon')
+const tooltip = document.querySelector('.tooltip')
+Popper.createPopper(buttonIcon, tooltip)
+
+buttonIcon.onclick = () => {
+    tooltip.classList.toggle('shown')
+}
+
+// end show icon chat 
+
+// insert icon chat 
+
+document.querySelector('emoji-picker').addEventListener('emoji-click', e => {
+    textFieldEdit.insert(document.querySelector('.input-bar input'), e.detail.unicode)
+})
+//end insert icon chat 
