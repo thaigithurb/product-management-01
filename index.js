2fe666e2b13a05183611fac18d1414cc2355487a
thaigithurb <thaivietluong2005@gmail.com>
2025-11-03 14:24:57 +0700

    cài đặt socket.io vào project

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const flash = require('express-flash');
-const multer  = require('multer');
+const multer = require('multer');
 const path = require('path');
+const {
+  createServer
+} = require('node:http');
+const {
+  join
+} = require('node:path');
+const {
+  Server
+} = require('socket.io');
 require('dotenv').config();
 
 const database = require("./config/database");
@@ -21,17 +30,33 @@ database.connect();
 const app = express();
 const port = process.env.PORT;
 
+// socket.io 
+const server = createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
+
+});
+
+
 app.use(methodOverride('_method'));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
-app.set('views', `${__dirname}/views`) ;
+app.set('views', `${__dirname}/views`);
 app.set('view engine', 'pug');
 
 // flash 
 app.use(cookieParser('ABDSLKFNLA'));
-app.use(session({ cookie: { maxAge: 60000 }}));
+app.use(session({
+  cookie: {
+    maxAge: 60000
+  }
+}));
 app.use(flash());
 // end flash 
 
@@ -53,6 +78,6 @@ app.use((req, res, next) => {
   });
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
